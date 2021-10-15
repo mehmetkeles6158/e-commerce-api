c841548f879b478e72e04608373a54485d7c11c0
Mehmet <mehmetk6158@gmail.com>
2021-10-14 22:07:07 -0400

    REST create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,13 +11,20 @@ class ProductsController < ApplicationController
   def show
 
     product = Product.find_by(id:2)
-
     the_id = params["id"]
-
     product = Product.find_by(id:the_id)
 
     render json:product.as_json
 
+  end
+
+  def create
+
+    new_product = Product.new(name:"IPAD PRO", price:799, image_url:"table.png", description:"it is a cool tablet for sketching your projects professionally")
+
+    new_product.save
+
+    render json: new_product.as_json
     
   end
 
